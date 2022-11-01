2824e8e70d1b81939f1fe8d35c26b337d64ad2ec
alejandroguerravillegas <coreartalex2@gmail.com>
2022-11-01 07:40:06 -0700

    .

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,13 +2,11 @@ const mongoose = require('mongoose');
 const  dotenv = require('dotenv');
 
 const connectDatabase = () => {
-    mongoose.connect(process.env.DB_LOCAL_URI, {
+    mongoose.connect(process.env.DATABASE_LOCAL_URI, {
         useNewUrlParser: true,
-        // useUnifieldTopology: true,
-        // useCreateIndex: true
     }).then(con => {
         console.log(`MongoDB Database Connected with HOST: ${con.connection.host}`);
-    })
-}
+    });
+};
 
 module.exports = connectDatabase
