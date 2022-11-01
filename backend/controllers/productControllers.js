908f783a49bf8ae48d930a0404a8c92adabc208b
alejandroguerravillegas <coreartalex2@gmail.com>
2022-10-31 21:48:15 -0700

    Get Route for All & Single Product & Test on Postman

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -9,11 +9,31 @@ exports.newProduct = async (req, res, next) => {
     })
 }
 
-exports.getProducts = (req,res,next) => {
+exports.getProducts = async (req,res,next) => {
 
-res.status(200).json({
-success: true,
-message: 'this route will show all products in db.'
-})
+    const products = await Product.find();
+        
+    res.status(200).json({
+    success: true,
+    count: products.length,
+    products
+    })
 
+}
+
+exports.getSingleProduct = async (req,res,next) => {
+
+    const product = await Product.findById(req.params.id);
+
+    if(!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found'
+        })
+    }
+
+    res.status(200).json({
+        success: true,
+        product
+    })
 }
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,8 +1,9 @@
 const express= require('express')
 const router = express.Router();
-const {getProducts, newProduct} = require('../controllers/productControllers');
+const {getProducts, newProduct, getSingleProduct} = require('../controllers/productControllers');
 
 router.route('/products').get(getProducts);
+router.route('/products/:id').get(getSingleProduct);
 router.route('/products/new').post(newProduct);
 
 module.exports = router;
