807c896f9246e0ce81f59fbd55b9e45bed135fd7
eacamposh <eacamposh@gmail.com>
2022-11-04 23:24:03 -0700

    Created function delete product

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -57,4 +57,21 @@ exports.updateProduct = async (req, res, next) => {
   });
 };
 
+// Delete Product
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
 
+  if (!product) {
+    return res.status(404).json({
+      success: false,
+      message: "Product not found",
+    });
+  }
+
+  await product.deleteOnev ();
+
+  res.status(200).json({
+    success: true,
+    message: "Product is deleted.",
+  });
+};
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -5,12 +5,13 @@ const {
   newProduct,
   getSingleProduct,
   updateProduct,
+  deleteProduct,
 } = require("../controllers/productControllers");
 
 router.route("/products").get(getProducts);
 router.route("/products/:id").get(getSingleProduct);
 router.route("/products/new").post(newProduct);
 
-router.route("/admin/products/:id").put(updateProduct);
+router.route("/admin/products/:id").put(updateProduct).delete(deleteProduct);
 
 module.exports = router;
