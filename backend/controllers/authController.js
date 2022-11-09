bfea151438932be2b2472c5e284e13724c2a7a25
eacamposh <eacamposh@gmail.com>
2022-11-09 12:23:08 -0800

    Add Sending and saved JWT token in cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middlewares/catchAsyncErrors");
+const sendToken = require("../utils/jwtToken");
 
 //Register a user
 
@@ -17,12 +18,8 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
     },
   });
 
-  const token = user.getJwtToken();
+  sendToken(user, 200, res);
 
-  res.status(201).json({
-    success: true,
-    token,
-  });
 });
 
 //* Login user
@@ -48,10 +45,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
     return next(new ErrorHandler("Invalid email or password", 401));
   }
 
-  const token = user.getJwtToken();
-
-  res.status(200).json({
-    success: true,
-    token,
-  });
+  sendToken(user, 200, res);
 });
