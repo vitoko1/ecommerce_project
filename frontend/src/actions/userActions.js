85f3cc75ade2f014274cffcb25adc8ee62cb2ee8
eacamposh <eacamposh@gmail.com>
2022-11-23 21:59:14 -0800

    Created Register user with reducer and action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -5,6 +5,9 @@ import {
   LOGIN_REQUEST,
   LOGIN_SUCCESS,
   LOGIN_FAIL,
+  REGISTER_USER_REQUEST,
+  REGISTER_USER_SUCCESS,
+  REGISTER_USER_FAIL,
   CLEAR_ERRORS,
 } from "../constants/userConstants";
 
@@ -35,7 +38,36 @@ export const login = (email, password) => async (dispatch) => {
   }
 };
 
-// clear errors
+//* Register user
+export const register = (userData) => async (dispatch) => {
+  try {
+
+      dispatch({ type: REGISTER_USER_REQUEST })
+
+      const config = {
+          headers: {
+              'Content-Type': 'multipart/form-data'
+          }
+      }
+
+      const { data } = await axios.post('/api/v1/register', userData, config)
+
+      dispatch({
+          type: REGISTER_USER_SUCCESS,
+          payload: data.user
+      })
+
+  } catch (error) {
+      dispatch({
+          type: REGISTER_USER_FAIL,
+          payload: error.response.data.message
+      })
+  }
+}
+
+
+
+//* clear errors
 export const clearErrors = () => async (dispatch) => {
   dispatch({
     type: CLEAR_ERRORS,
