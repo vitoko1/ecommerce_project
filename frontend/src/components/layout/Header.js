61dd5b01949bd1af6aedcec03cb4992e64e38d68
eacamposh <eacamposh@gmail.com>
2022-11-24 21:55:12 -0800

    Add Logout User

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -6,12 +6,18 @@ import "../../App.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { useAlert } from "react-alert";
+import { logout } from "../../actions/userActions";
 
 const Header = () => {
   const alert = useAlert();
   const dispatch = useDispatch();
 
   const { user, loading } = useSelector((state) => state.auth);
+
+  const logoutHandler = () => {
+    dispatch(logout());
+    alert.success("Logged out successfully!");
+  };
 
   return (
     <Fragment>
@@ -78,7 +84,11 @@ const Header = () => {
                 <Link className="dropdown-item" to="/me">
                   Profile
                 </Link>
-                <Link className="dropdown-item text-danger" to="/">
+                <Link
+                  className="dropdown-item text-danger"
+                  to="/"
+                  onClick={logoutHandler}
+                >
                   Logout
                 </Link>
               </div>
